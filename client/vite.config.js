3c6a044c9ba29dedd35f11ee24bd0002e4add0f0
Imani <imani.james@student.moringaschool.com>
2025-07-01 08:07:10 +0300

    Fix: Added base './' to vite.config.js to resolve Netlify deployment issue

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: './', // ✅ use './' to ensure relative paths
   plugins: [react()],
 })
